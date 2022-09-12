61927bb30bcd1c57f0809cc7bcaa202114e96f44
brebeneladrian <brebenel.adrian@yahoo.com>
2022-09-12 13:59:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-@Input()   onStartScale();
-
 export class AppComponent {
   // alerts: Alert[];
   message: string = 'Prototype of a line-machine !';
